46276cf86955cdce0c3610d085697fc1bc75a690
vsilwal <vipulsilwal@gmail.com>
2025-04-10 19:01:45 +0530

    spg workshop

---
+++ SPGIITR2025/script.js
@@ -0,0 +1,16 @@
+const sections = document.querySelectorAll('.section');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
+}, {
+  threshold: 0.1
+});
+
+sections.forEach(section => {
+  section.classList.add('hidden');
+  observer.observe(section);
+});
